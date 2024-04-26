26d0de568766b650dfc47f44e8bffe0f5849838a
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-04-26 22:13:33 +0530

    completed

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+       <h1> Hey! Kalvian  Welcome to Contact Page </h1>
+      <h1>This is Contact Page</h1>
+  
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1> Hey! Kalvian Welcome to home Page </h1>
+      <h1>This is Home Page</h1>
+   
+
+
+      
+    </div>
+  )
+}
+
+export default Home
